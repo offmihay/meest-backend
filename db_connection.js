876d9f4eca27894c8b2dabee08c70cc923c713c8
Volodymyr <offmihay@gmail.com>
2024-05-11 18:56:26 +0300

    rename env config vars

--- db_connection.js
+++ db_connection.js
@@ -2,17 +2,16 @@ const config = require("./config");
 const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DEVELOPMENT_DATABASE_NAME,
-  process.env.DEVELOPMENT_DATABASE_USER,
-  process.env.DEVELOPMENT_DATABASE_PASSWORD,
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
   {
     dialect: "postgres",
-    host: process.env.DEVELOPMENT_DATABASE_HOST,
-    port: process.env.DEVELOPMENT_DATABASE_PORT,
-    waitForConnections:
-      process.env.DEVELOPMENT_DATABASE_WAITFORCONNECTIONS === "true", // Перетворення рядка в логічне значення
-    connectionLimit: parseInt(process.env.DEVELOPMENT_DATABASE_CONNECTIONLIMIT),
-    queueLimit: parseInt(process.env.DEVELOPMENT_DATABASE_QUEUELIMIT),
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    waitForConnections: process.env.DATABASE_WAITFORCONNECTIONS === "true", // Перетворення рядка в логічне значення
+    connectionLimit: parseInt(process.env.DATABASE_CONNECTIONLIMIT),
+    queueLimit: parseInt(process.env.DATABASE_QUEUELIMIT),
   }
 );
 
