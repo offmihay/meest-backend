b9f4a9baec8329c4008e3c23cb9e416f940e1b61
Volodymyr <offmihay@gmail.com>
2024-05-11 19:15:26 +0300

    fix2

--- db_connection.js
+++ db_connection.js
@@ -6,6 +6,11 @@ const sequelize = new Sequelize(
   process.env.DATABASE_PASSWORD,
   {
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: process.env.DATABASE_SSL === "true",
+      },
+    },
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
     waitForConnections: process.env.DATABASE_WAITFORCONNECTIONS === "true", // Перетворення рядка в логічне значення
