4594b28b956f2f37438600d5251ca88bd853bbf4
Volodymyr <offmihay@gmail.com>
2024-05-11 18:58:29 +0300

    uncommited

--- routesv1.js
+++ routesv1.js
@@ -3,13 +3,13 @@ require("express-async-errors");
 const router = express.Router();
 const bodyParser = require("body-parser");
 const errorHandler = require("./middlewares/error_handler");
-// const allBrandsHandler = require("./handlers/all_brands");
-// const brandsHandler = require("./handlers/brands");
-// const clothesHandler = require("./handlers/clothes");
+const allBrandsHandler = require("./handlers/all_brands");
+const brandsHandler = require("./handlers/brands");
+const clothesHandler = require("./handlers/clothes");
 
-// const sequelize = require("./db_connection");
-// const initModels = require("./models/init-models");
-// const models = initModels(sequelize);
+const sequelize = require("./db_connection");
+const initModels = require("./models/init-models");
+const models = initModels(sequelize);
 
 const cors = require("cors");
 router.use(cors());
@@ -17,17 +17,17 @@ router.use(cors());
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
-console.log('hello world')
+console.log("hello world");
 
-// router.get("/all-brands", (req, res, next) =>
-//   allBrandsHandler(req, res, next, models)
-// );
-// router.get("/brands", (req, res, next) =>
-//   brandsHandler(req, res, next, models)
-// );
-// router.get("/clothes", (req, res, next) =>
-//   clothesHandler(req, res, next, models)
-// );
+router.get("/all-brands", (req, res, next) =>
+  allBrandsHandler(req, res, next, models)
+);
+router.get("/brands", (req, res, next) =>
+  brandsHandler(req, res, next, models)
+);
+router.get("/clothes", (req, res, next) =>
+  clothesHandler(req, res, next, models)
+);
 
 router.use(errorHandler);
 
