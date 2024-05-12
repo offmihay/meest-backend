cc34c19924c7dbd3fc494adb8fbd7c45dd1e6a0e
Volodymyr <offmihay@gmail.com>
2024-05-12 16:26:21 +0300

    added login req

--- routesv1.js
+++ routesv1.js
@@ -3,9 +3,6 @@ require("express-async-errors");
 const router = express.Router();
 const bodyParser = require("body-parser");
 const errorHandler = require("./middlewares/error_handler");
-const allBrandsHandler = require("./handlers/all_brands");
-const brandsHandler = require("./handlers/brands");
-const clothesHandler = require("./handlers/clothes");
 
 const sequelize = require("./db_connection");
 const initModels = require("./models/init-models");
@@ -17,6 +14,10 @@ router.use(cors());
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
+const allBrandsHandler = require("./handlers/all_brands");
+const brandsHandler = require("./handlers/brands");
+const clothesHandler = require("./handlers/clothes");
+const loginHandler = require("./handlers/login");
 
 router.get("/all-brands", (req, res, next) =>
   allBrandsHandler(req, res, next, models)
@@ -25,6 +26,9 @@ router.get("/brands", (req, res, next) =>
   brandsHandler(req, res, next, models)
 );
 router.get("/clothes", (req, res, next) =>
+  clothesHandler(req, res, next, models)
+);
+router.post("/login", (req, res, next) =>
   clothesHandler(req, res, next, models)
 );
 
