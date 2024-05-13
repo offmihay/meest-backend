42e0ea50d8a5ffbe95143522bb1d71620f91e481
Volodymyr <offmihay@gmail.com>
2024-05-13 19:16:31 +0300

    fix code login.js

--- handlers/login.js
+++ handlers/login.js
@@ -6,25 +6,26 @@ module.exports = async (req, res, next, models) => {
 
   const user = await models.users.findOne({ where: { username } });
   if (!user) {
-    return res.status(404).json({ error: "User not found" });
+    return res.status(401).json({ error: "Invalid credentials" });
   }
 
-  bcrypt.compare(password, user.password_hash, async (err, isPasswordValid) => {
-    if (err) {
-      return res.status(500).json("Internal server error");
-    }
-    if (isPasswordValid) {
-      const token = jwt.sign(
-        { id: user.id, username: user.username },
-        process.env.JWT_SECRET
-      );
-      await models.users.update(
-        { last_login_date: new Date() },
-        { where: { id: user.id } }
-      );
-      res.json({ userData: { userID: user.id, token: `${token}` } });
-    } else {
-      res.status(401).json("Invalid credentials");
-    }
-  });
+  const theSame = await bcrypt.compare(password, user.password_hash);
+
+  if (!theSame) {
+    res.status(401).json({ error: "Invalid credentials" });
+    return;
+  }
+
+  const token = jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.JWT_SECRET
+  );
+
+  await models.users.update(
+    { last_login_date: new Date() },
+    { where: { id: user.id } }
+  );
+
+  res.json({ userData: { userID: user.id, token: `${token}` } });
+
 };
